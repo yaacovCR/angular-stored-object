638de32852070cf80844f9c4b7b637de9fda4d5d
yaacovCR <yaacovCR@gmail.com>
2016-03-23 08:54:04 -0400

    Initial docs run

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,54 @@
+NG_DOCS={
+  "sections": {
+    "api": "angular-stored-object"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "yaacovCR.storedObject",
+      "shortName": "yaacovCR.storedObject",
+      "type": "overview",
+      "moduleName": "yaacovCR.storedObject",
+      "shortDescription": "yaacovCR.storedObject",
+      "keywords": "api html5 interaction module objects overview service storage storedobject support yaacovcr ycr"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api/yaacovCR.storedObject",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/node_modules/angular/angular.min.js",
+      "js/node_modules/angular-animate/angular-animate.min.js",
+      "js/node_modules/marked/lib/marked.js"
+    ],
+    "styles": [],
+    "title": "angular-stored-object",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api/yaacovCR.storedObject",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "js/node_modules/angular/angular.min.js",
+    "js/node_modules/angular-animate/angular-animate.min.js",
+    "js/node_modules/marked/lib/marked.js"
+  ]
+};
